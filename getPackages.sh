0775de3f11e1e8d7cdc758a08f79e6ec42014bea
Tontsu <tontsu@pop-os.localdomain>
2020-08-18 16:43:33 +0300

    remove architecture from getPackages

--- getPackages.sh
+++ getPackages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dpkg-query -f 'Package: ${binary:Package}\nStatus: ${Status}\nPriority: ${Priority}\nSection: ${Section}\nDepends: ${Depends}\nDescription: ${Description}\n\n' -W > status.real
+dpkg-query -f 'Package: ${Package}\nStatus: ${Status}\nPriority: ${Priority}\nSection: ${Section}\nDepends: ${Depends}\nDescription: ${Description}\n\n' -W > status.real
 sed -i '/^Depends: $/d' status.real
